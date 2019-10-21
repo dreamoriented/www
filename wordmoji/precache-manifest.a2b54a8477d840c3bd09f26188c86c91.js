52ddabea52c5a576b3e31744a8f85ed2489601a9
Burak Tokak <iletisim@buraktokak.com>
2019-10-21 05:52:31 +0300

    Add files via upload

---
+++ wordmoji/precache-manifest.a2b54a8477d840c3bd09f26188c86c91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c9e5288889bbe80b6a11439b21ede6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3d74e5e87641cd95a2a",
+    "url": "./static/css/main.5bb6d6e9.chunk.css"
+  },
+  {
+    "revision": "23407447baf71a8a5f72",
+    "url": "./static/js/2.3a6736e1.chunk.js"
+  },
+  {
+    "revision": "f3d74e5e87641cd95a2a",
+    "url": "./static/js/main.49a799db.chunk.js"
+  },
+  {
+    "revision": "7139c3515a6eaae4baa2",
+    "url": "./static/js/runtime-main.9a9db2be.js"
+  },
+  {
+    "revision": "5b5866f8e603bcb82b6702eab1678b45",
+    "url": "./static/media/as.5b5866f8.svg"
+  },
+  {
+    "revision": "7bb4486708f6e20e8a8da878944112a9",
+    "url": "./static/media/gp.7bb44867.svg"
+  },
+  {
+    "revision": "41255d707a45af8a49831164e439ef4a",
+    "url": "./static/media/icon.41255d70.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
--- wordmoji/service-worker.js
+++ wordmoji/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a7495638ccf8f542018a91a9a3431cd.js"
+  "./precache-manifest.a2b54a8477d840c3bd09f26188c86c91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
