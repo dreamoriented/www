b4603636932ccefc2ad23ff56e09c6a5eb85b3bb
Burak Tokak <iletisim@buraktokak.com>
2020-01-08 09:23:33 +0300

    add mola

---
+++ mola/precache-manifest.031555364ff4df1239cb36626c198ce1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118adf7e1a4fefb0d9bf954bc3a89f8e",
+    "url": "./mola/index.html"
+  },
+  {
+    "revision": "8d911e7c993e82fc5ae5",
+    "url": "./mola/static/css/main.5bb6d6e9.chunk.css"
+  },
+  {
+    "revision": "23407447baf71a8a5f72",
+    "url": "./mola/static/js/2.3a6736e1.chunk.js"
+  },
+  {
+    "revision": "8d911e7c993e82fc5ae5",
+    "url": "./mola/static/js/main.4048c128.chunk.js"
+  },
+  {
+    "revision": "240df50051eefa644047",
+    "url": "./mola/static/js/runtime-main.4125d796.js"
+  },
+  {
+    "revision": "5b5866f8e603bcb82b6702eab1678b45",
+    "url": "./mola/static/media/as.5b5866f8.svg"
+  },
+  {
+    "revision": "7bb4486708f6e20e8a8da878944112a9",
+    "url": "./mola/static/media/gp.7bb44867.svg"
+  },
+  {
+    "revision": "1def5f140cc038729323bedb34d78ff1",
+    "url": "./mola/static/media/icon.1def5f14.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./mola/static/media/logo.25bf045c.svg"
+  }
+]);
