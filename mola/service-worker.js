f6c0579882f797f54cbb26bc6814371acf7fe560
Burak Tokak <iletisim@buraktokak.com>
2020-01-08 09:25:58 +0300

    Add files via upload

--- mola/service-worker.js
+++ mola/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./mola/precache-manifest.031555364ff4df1239cb36626c198ce1.js"
+  "./precache-manifest.5ae70ee95386095948e82b728b8e8ada.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./mola/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
